8e1b30c01538e11d9287266fdbf71c9bc4ed0d61
Drew Schrauf <andrew.schrauf@gmail.com>
2016-04-04 20:31:33 +1000

    Prep for travis

--- src/AcidPluginBlog.js
+++ src/AcidPluginBlog.js
@@ -88,7 +88,7 @@ export default function(options) {
                     return Object.keys(routes);
                 });
             },
-            resolveTemplate: url => {
+            resolveTemplate: () => {
                 return path.join(templateDir, 'post.marko');
             }
         }
